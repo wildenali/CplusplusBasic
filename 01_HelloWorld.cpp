ffd4cedb41156297c1c6941425f4b86614d00269
Wilden Ali <wildeeeeen@gmail.com>
2018-08-09 21:43:17 +0700

    Add files via upload

---
+++ 01_HelloWorld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+  std::cout << "hay gaays" << std::endl;
+  std::cin.get();
+  return 0;
+}
+
+// Nah cara menjalankannya adalah
+// 1. Buka Terminal
+// 2. Masuk ke dalam directory si File ini
+// 3. Tuliskan g++ 01_HelloWorld.cpp
+// 4. nah nanti akan menghasilkan file baru (biasanya a.out)
+// 5. si a.out ini file apps nya, buat mengeksekusinya caranya
+// 6. Ketik ./a.out
+// 7. lalu ENTER
+// 8. Keluar hasilnya di terminal
