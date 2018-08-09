1e302ed2394fa1cad81471c381b8170f7d2c23a3
Wilden Ali <wildeeeeen@gmail.com>
2018-08-09 22:06:08 +0700

    Add files via upload

---
+++ 02_HelloWorld2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "hay gaays pake namespace std nih";
+  return 0;
+}
+
+// Nah cara menjalankannya adalah
+// 1. Buka Terminal
+// 2. Masuk ke dalam directory si File ini
+// 3. Tuliskan g++ 01_HelloWorld2.cpp
+// 4. nah nanti akan menghasilkan file baru (biasanya a.out)
+// 5. si a.out ini file apps nya, buat mengeksekusinya caranya
+// 6. Ketik ./a.out
+// 7. lalu ENTER
+// 8. Keluar hasilnya di terminal
