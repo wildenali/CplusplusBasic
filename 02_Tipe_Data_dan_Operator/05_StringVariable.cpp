4fd5ce083925b0746d2acdca5df5f7369d0cfb09
wildenali <wildeeeeen@gmail.com>
2018-11-27 22:25:52 +0700

    tipe data dan operator-operator

---
+++ 02_Tipe_Data_dan_Operator/05_StringVariable.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>   // kalau mau pakai string, include kan dulu si string header nya
+using namespace std;
+
+int main() {
+  string teststring = "hallow apa kabar";
+  cout <<  teststring << endl;
+
+  return 0;
+}
