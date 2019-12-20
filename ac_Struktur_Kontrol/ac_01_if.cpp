c4f0f56a34e9bee1f95bdc7db64fdfd69196f66b
wildenali <wildeeeeen@gmail.com>
2019-12-20 15:09:36 +0700

    Pengandaian Jik -If-

---
+++ ac_Struktur_Kontrol/ac_01_if.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+    double a, b, c;
+
+    std::cout << "Masukan Nilai a: ";
+    std::cin >> a;
+    std::cout << "Masukan Nilai b: ";
+    std::cin >> b;
+
+    
+    if (b == 0) {
+        std::cout << "Tidak bisa di bagi NOL dong, nanti hasilnya tak hingga" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    c = a / b;
+    std::cout << a << " di BAGI dengan " << b << " sama dengan "
+              << c
+              << std::endl;
+
+    return 0;
+    
+}
