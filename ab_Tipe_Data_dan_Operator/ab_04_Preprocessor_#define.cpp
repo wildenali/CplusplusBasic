5f187886570ffd9e87a2c844a81aaf66f554df7c
GalihBaskara <1galihbaskara@gmail.com>
2019-12-20 13:39:22 +0700

    preprocessor atau hastag define #define blabla

---
+++ ab_Tipe_Data_dan_Operator/ab_04_Preprocessor_#define.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+preprocessor definitions adalah cara kita memberi
+nama pada sebuah nilai yang konstan
+*/
+#define panjang 4
+#define lebar 5
+
+int main() {
+    int luas;
+    luas = panjang * lebar;
+    
+    cout << luas << endl;
+
+    return 0;
+}
