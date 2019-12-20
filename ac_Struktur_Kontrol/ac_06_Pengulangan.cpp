af5bf1c227dd3442493459ecdb3db2b919e41bf4
wildenali <wildeeeeen@gmail.com>
2019-12-20 16:30:07 +0700

    pengulangan FOR to DO, WHILE DO, DO WHILE, FOR BREAK, FOR CONTINUE

--- ac_Struktur_Kontrol/ac_06_Pengulangan.cpp
+++ ac_Struktur_Kontrol/ac_06_Pengulangan.cpp
@@ -11,10 +11,46 @@ int main() {
 
     std::cout << "===== WHILE DO =====" << '\n';
     int b = 5;
-    while(b <= 15){
+    while(b < 15){
         std::cout << b << '\n';
         b++;
     }
     std::cout << '\n';
+
+    std::cout << "===== DO WHILE =====" << '\n';
+    int c = 5;
+    do
+    {
+        std::cout << c << '\n';
+        c++;
+    } while (c < 15);
+    std::cout << '\n';
+
+    std::cout << "===== FOR BREAK =====" << '\n';
+    for(int d = 5; d <= 15; d++)
+    {
+        std::cout << d << '\n';
+        if (d == 35) {
+            break;
+        }
+    }
+    std::cout << '\n';
+
+    std::cout << "===== FOR CONTINUE =====" << '\n';
+    for(int e = 40; e < 50; e++)
+    {
+        if (e % 2 == 1) {   // ketika didapati bilangan ganjil, maka dilewati
+            continue;
+        }
+        if (e == 42) {  // ketika ketemu angka 42, di lewati
+            continue;
+        }   
+        std::cout << e << '\n';
+    }
+    std::cout << '\n';
+
+
+
+    return 0;
     
 }
