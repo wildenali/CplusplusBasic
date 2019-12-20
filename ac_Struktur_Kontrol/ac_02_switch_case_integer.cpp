3a01ca2eee47fd5318958e047c512a446099ba7a
wildenali <wildeeeeen@gmail.com>
2019-12-20 15:28:56 +0700

    switch case dengan contoh kasus input bilangan integer

---
+++ ac_Struktur_Kontrol/ac_02_switch_case_integer.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+
+    int nohari;
+    std::string hari;
+    
+    std::cout << "Masukan no hari: ";
+    std::cin >> nohari;
+
+    switch (nohari)
+    {
+        case 1: hari = "Minggu";
+            break;
+        case 2: hari = "Senin";
+            break;
+        case 3: hari = "Selasa";
+            break;
+        case 4: hari = "Rabu";
+            break;
+        case 5: hari = "Kamis";
+            break;
+        case 6: hari = "Jumat";
+            break;
+        case 7: hari = "Sabtu";
+            break;
+        default:
+            std::cout << "Tidak ada Hari ke -"
+                      << nohari
+                      << "- adanya cuma 1 sampai 7"
+                      << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Hari ke-" << nohari
+              << " adalah " << hari
+              << std::endl;
+
+
+    return 0;
+}
