f4f2aa05d8def3c542f7eacb2b64e92dffe774e6
GalihBaskara <1galihbaskara@gmail.com>
2019-12-20 13:33:16 +0700

    test string

---
+++ ab_Tipe_Data_dan_Operator/ab_03_StringVariable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>       // panggil <string> untuk akses string nya
+
+using namespace std;
+
+int main() {
+    string teststring = "halow apa kabar";
+    cout << teststring << endl;
+
+    return 0;
+}
