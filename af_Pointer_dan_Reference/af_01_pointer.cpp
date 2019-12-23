727a0d45219b7aab7d4b0decc04c80bf12891166
wildenali <wildeeeeen@gmail.com>
2019-12-23 11:18:43 +0700

    Reference(*) adalah operator untuk MENYATAKAN sebuah variable adalah VARIABLE POINTER

--- af_Pointer_dan_Reference/af_01_pointer.cpp
+++ af_Pointer_dan_Reference/af_01_pointer.cpp
@@ -3,8 +3,25 @@
 using namespace std;
 
 int main() {
-    int a = 5;
+    /*
+    Reference (*) merupakan suatu operator yang berfungsi MENYATAKAN
+    suatu variabel adalah VARIABLE POINTER.
+    Peletakan simbol operator reference diletakan diawal variabel.
+    Operator reference ini akan membuat suatu variabel pointer untuk
+    menampung alamat.
+    */
 
+    int x = 5;  // Memberikan nilai 5 pada variable x
+    int *y;     // Mendeklarasikan variable y sebagai pointer
+    y = &x;     // Mengkopikan alamat variable x ke dalam variable pointer b
+    cout << "Nilai variable x                       : " << x << endl;
+    cout << "Alamat variable x                      : " << &x << endl;
+    cout << "Isi dari variable y                    : " << y << endl;
+    cout << "Nilai yang tertampung dalam variable b : " << *y << endl;
+
+
+
+    int a = 5;
     // pointer
     int *aPtr = nullptr;
     aPtr = &a;      // isi aPtr adalah alamat dari a
