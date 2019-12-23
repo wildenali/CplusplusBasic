a507e42f28e0ddbbf12c0c35f5d61792aac6c441
wildenali <wildeeeeen@gmail.com>
2019-12-23 11:42:09 +0700

    Penggunaan pointer dalam Fungsi

---
+++ af_Pointer_dan_Reference/af_03_Fungsi_dengan_pointer.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+void fungsi(int *);
+void kuadrat(int *);
+
+int main() {
+    int a = 5;
+    cout << "address a : " << &a << endl;
+    cout << "nilai a   : " << a << endl << endl;
+
+    fungsi(&a); //fungsi dengan input pointer
+    cout << endl;
+    kuadrat(&a);
+ 
+    cout << "nilai a   : " << a << endl;
+
+    cin.get();
+
+    return 0;
+}
+
+void fungsi(int *b) {
+    cout << "address b : " << b << endl;
+    cout << "nilai b   : " << *b << endl;       // dereferencing
+}
+
+void kuadrat(int *valPtr) {
+    *valPtr = (*valPtr) * (*valPtr);
+}
