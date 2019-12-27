f29ccf61f1190377155df2acf47d3c1396f328c2
wildenali <wildeeeeen@gmail.com>
2019-12-27 21:06:54 +0700

    ngebandingkan string

---
+++ ag_String/ag_03_perbandingan_string.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    // Perbandingan string
+    string input;
+    string nama_rahasia("ucup");
+
+    while(true){
+        cout << "tebak nama : ";
+        cin >> input;
+        cout << input;
+        if(input == nama_rahasia){
+            cout << " tebakan anda benar!!!!!!!" << endl;
+            break;
+        }
+        cout << "tebakan anda salah" << endl;
+    }
+    cout << "program selesai" << endl;
+
+    cin.get();
+    return 0;
+}
