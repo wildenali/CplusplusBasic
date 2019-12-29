b6b528a79c9304c4f2b400dd0974bb1d7b60453e
wildenali <wildeeeeen@gmail.com>
2019-12-29 09:09:34 +0700

    membuat struct yang bersarang

--- ah_Struct/ah_02_Nesting_Struct.cpp
+++ ah_Struct/ah_02_Nesting_Struct.cpp
@@ -31,7 +31,7 @@ int main(){
     aktor2.tahun_lahir = 1995;
 
     // buat film
-    film1.judul = "Pengabdi Wakanda";
+    film1.judul = "Pengabdi Wakanda Forever";
     film1.genre = "Documenter";
     film1.tahun = 2019;
     film1.pemeran_1 = aktor1;
