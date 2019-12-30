64bbbade3a2953caf47ec634b7006f6a533c48ad
wildenali <wildeeeeen@gmail.com>
2019-12-30 22:12:25 +0700

    preprocessing itu misal #define apaApaMisalnya

---
+++ ar_Preprocessing/ar_01_Preprocessing.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#define PI 3.14159265359
+
+using namespace std;
+
+double pi = 3.14159265359;
+
+int main(){
+    cout << " nilai pi double " << pi << endl;
+    cout << &pi << endl;
+    cout << "nilai PI = " << PI << endl;
+
+    return 0;
+}
